6946d040057b7bc6086b60770b58eac117124855
Christopher Gerber <christopher.gerber@changehealthcare.com>
2019-05-09 13:08:44 -0500

    Cleans up "project" resource schema and testing framework
    Uses the *gitlab.Project struct and resource Schema to test the
    resulting values.
    
    removes the extra generate access token file and updates the access
    token value to be generated from a variable (with a default).

--- scripts/start-gitlab.sh
+++ scripts/start-gitlab.sh
@@ -1,16 +1,31 @@
 #!/bin/bash -e
-if [[ $MAKE_TARGET != "testacc" ]]; then echo "not starting gitlab!"; exit 0; fi
+test "$MAKE_TARGET" == "testacc" || { echo "not starting gitlab!"; exit 0; }
 echo "Starting gitlab container..."
-docker run -d -e GITLAB_ROOT_PASSWORD=adminadmin --rm -p 127.0.0.1:8080:80 --name gitlab gitlab/gitlab-ce
+docker run -d --rm --name gitlab \
+  -e GITLAB_ROOT_PASSWORD=adminadmin \
+  -p 127.0.0.1:8080:80 \
+  gitlab/gitlab-ce
+
 echo -n "Waiting for gitlab to be ready "
 i=1
-until wget -t 1 127.0.0.1:8080 -O /dev/null -q 
+until wget -t 1 127.0.0.1:8080 -O /dev/null -q
 do
     sleep 1
     echo -n .
     if [[ $((i%3)) == 0 ]]; then echo -n ' '; fi
-    let i++
+    (( i++ ))
 done
+
 echo
 echo "Creating access token"
-docker exec -i gitlab gitlab-rails console < scripts/generate-access-token.rb
+(
+  echo -n 'terraform_token = PersonalAccessToken.create('
+  echo -n 'user_id: 1, '
+  echo -n 'scopes: [:api, :read_user], '
+  echo -n 'name: :terraform);'
+  echo -n "terraform_token.set_token('${GITLAB_TOKEN:=ACCTEST}');"
+  echo -n 'terraform_token.save!;'
+) |
+docker exec -i gitlab gitlab-rails console
+
+
