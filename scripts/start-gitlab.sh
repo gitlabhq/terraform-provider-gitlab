2e423624e954947a1e3e6ab204814ab9ba901714
Julien Pivotto <roidelapluie@inuits.eu>
2019-10-16 23:45:05 +0200

    Test EE (#188)
    * Test EE
    
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>
    
    * Fix start
    
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>
    
    * Add chmod
    
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>
    
    * Add debug
    
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>
    
    * Add volume
    
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

--- scripts/start-gitlab.sh
+++ scripts/start-gitlab.sh
@@ -1,10 +1,18 @@
 #!/bin/bash -e
 test "$MAKE_TARGET" == "testacc" || { echo "not starting gitlab!"; exit 0; }
 echo "Starting gitlab container..."
+if [[ -n $GITLAB_LICENSE_FILE ]]
+then
+    extra="-v $PWD/license:/license -e GITLAB_LICENSE_FILE=/license/$GITLAB_LICENSE_FILE"
+    img=gitlab/gitlab-ee
+else
+    img=gitlab/gitlab-ce
+fi
 docker run -d --rm --name gitlab \
   -e GITLAB_ROOT_PASSWORD=adminadmin \
+  $extra \
   -p 127.0.0.1:8080:80 \
-  gitlab/gitlab-ce
+  $img
 
 echo -n "Waiting for gitlab to be ready "
 i=1
