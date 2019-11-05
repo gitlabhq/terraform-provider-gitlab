2ff9ba0f8325db9357a6d884246c27bb413409de
Chris Gerber <chris.gerber@well.co>
2019-11-04 20:41:47 -0600

    Updates tests & matrix, switching to testing GitLab EE
    Fixes broken EE tests. It looks like the license file was trying to write to a
    directory called `license/`, but was conflicting with the file named `LICENSE`.
    
    This switches the default tests to use GitLab EE rather than CE. The two
    installations are the same thing, but the presence of the license file enables
    additional features.
    
    From the GitLab info page:
    
    "This means that if you have installed GitLab Enterprise Edition without a
    license, you will not notice a difference with a typical Community Edition
    instance [...]"

--- scripts/license.sh
+++ scripts/license.sh
@@ -1,4 +1,19 @@
-mkdir license
-[[ -n $encrypted_4a4b21de8599_key ]] && echo decrypt
-[[ -n $encrypted_4a4b21de8599_key ]] && openssl aes-256-cbc -K $encrypted_4a4b21de8599_key -iv $encrypted_4a4b21de8599_iv -in JulienPivotto.gitlab-license.enc -out license/JulienPivotto.gitlab-license -d
-chmod 666 license/JulienPivotto.gitlab-license || true
+#!/bin/bash
+
+_license_dir=${GITLAB_LICENSE_DIR:-.license}
+_license_file=${GITLAB_LICENSE_FILE:-JulienPivotto.gitlab-license}
+
+mkdir -p "${_license_dir}";
+
+if test -n "$encrypted_4a4b21de8599_key"; then
+  echo decrypt
+
+  openssl aes-256-cbc \
+    -K "$encrypted_4a4b21de8599_key" \
+    -iv "$encrypted_4a4b21de8599_iv" \
+    -in "${_license_file}.enc" \
+    -out "${_license_dir}/${_license_file}" \
+    -d
+
+  chmod 666 "${_license_dir}/${_license_file}"
+fi
