716cbaae30df7735c8cdaa080a3b70f9ae9ff69d
Chris Gerber <chris.gerber@well.co>
2019-11-04 21:01:49 -0600

    Fixes license load script

--- scripts/license.sh
+++ scripts/license.sh
@@ -3,17 +3,8 @@
 _license_dir=${GITLAB_LICENSE_DIR:-.license}
 _license_file=${GITLAB_LICENSE_FILE:-JulienPivotto.gitlab-license}
 
-mkdir -p "${_license_dir}";
+mkdir -p "${_license_dir}"
 
-if test -n "$encrypted_4a4b21de8599_key"; then
-  echo decrypt
-
-  openssl aes-256-cbc \
-    -K "$encrypted_4a4b21de8599_key" \
-    -iv "$encrypted_4a4b21de8599_iv" \
-    -in "${_license_file}.enc" \
-    -out "${_license_dir}/${_license_file}" \
-    -d
-
-  chmod 666 "${_license_dir}/${_license_file}"
-fi
+[[ -n $encrypted_4a4b21de8599_key ]] && echo decrypt
+[[ -n $encrypted_4a4b21de8599_key ]] && openssl aes-256-cbc -K $encrypted_4a4b21de8599_key -iv $encrypted_4a4b21de8599_iv -in ${_license_file}.enc -out "${_license_dir}/${_license_file}" -d
+chmod 666 "${_license_dir}/${_license_file}" || true
