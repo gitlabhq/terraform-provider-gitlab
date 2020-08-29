240918bf72658462eed6a479f3aac4b7347bd2cc
Luke Griffith <lukemgriffith@gmail.com>
2020-08-29 12:08:20 +0100

    Adding project miorror and tests

---
+++ gitlab/resource_gitlab_project_mirror.go
@@ -0,0 +1,176 @@
+package gitlab
+
+import (
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/xanzy/go-gitlab"
+)
+
+func resourceGitlabProjectMirror() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceGitlabMirrorCreate,
+		Read:   resourceGitlabProjectMirrorRead,
+		Update: resourceGitlabMirrorUpdate,
+		Delete: resourceGitlabMirrorDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"project": {
+				Type:     schema.TypeString,
+				ForceNew: true,
+				Required: true,
+			},
+			"mirror_id": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"url": {
+				Type:     schema.TypeString,
+				ForceNew: true,
+				Required: true,
+			},
+			"enabled": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+			"only_protected_branches": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+			"keep_divergent_refs": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+		},
+	}
+}
+
+func resourceGitlabMirrorCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*gitlab.Client)
+
+	projectID := d.Get("project").(string)
+	URL := d.Get("url").(string)
+	enabled := d.Get("enabled").(bool)
+	onlyProtectedBranches := d.Get("only_protected_branches").(bool)
+	keepDivergentRefs := d.Get("keep_divergent_refs").(bool)
+
+	options := &gitlab.AddProjectMirrorOptions{
+		URL:                   &URL,
+		Enabled:               &enabled,
+		OnlyProtectedBranches: &onlyProtectedBranches,
+		KeepDivergentRefs:     &keepDivergentRefs,
+	}
+
+	log.Printf("[DEBUG] create gitlab project mirror for project %v", projectID)
+
+	mirror, _, err := client.ProjectMirrors.AddProjectMirror(projectID, options)
+	if err != nil {
+		return err
+	}
+	d.Set("mirror_id", mirror.ID)
+
+	mirrorID := strconv.Itoa(mirror.ID)
+	d.SetId(buildTwoPartID(&projectID, &mirrorID))
+	return resourceGitlabProjectMirrorRead(d, meta)
+}
+
+func resourceGitlabMirrorUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*gitlab.Client)
+
+	mirrorID := d.Get("mirror_id").(int)
+	projectID := d.Get("project").(string)
+	enabled := d.Get("enabled").(bool)
+	onlyProtectedBranches := d.Get("only_protected_branches").(bool)
+	keepDivergentRefs := d.Get("keep_divergent_refs").(bool)
+
+	options := gitlab.EditProjectMirrorOptions{
+		Enabled:               &enabled,
+		OnlyProtectedBranches: &onlyProtectedBranches,
+		KeepDivergentRefs:     &keepDivergentRefs,
+	}
+	log.Printf("[DEBUG] update gitlab project mirror %v for %s", mirrorID, projectID)
+
+	_, _, err := client.ProjectMirrors.EditProjectMirror(projectID, mirrorID, &options)
+	if err != nil {
+		return err
+	}
+	return resourceGitlabProjectMirrorRead(d, meta)
+}
+
+// Documented remote mirrors API does not support a delete method, instead mirror is disabled.
+func resourceGitlabMirrorDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*gitlab.Client)
+
+	enabled := false
+
+	mirrorID := d.Get("mirror_id").(int)
+	projectID := d.Get("project").(string)
+	onlyProtectedBranches := d.Get("only_protected_branches").(bool)
+	keepDivergentRefs := d.Get("keep_divergent_refs").(bool)
+
+	options := gitlab.EditProjectMirrorOptions{
+		Enabled:               &enabled,
+		OnlyProtectedBranches: &onlyProtectedBranches,
+		KeepDivergentRefs:     &keepDivergentRefs,
+	}
+	log.Printf("[DEBUG] Disable gitlab project mirror %v for %s", mirrorID, projectID)
+
+	_, _, err := client.ProjectMirrors.EditProjectMirror(projectID, mirrorID, &options)
+
+	return err
+}
+
+func resourceGitlabProjectMirrorRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*gitlab.Client)
+
+	ids := strings.Split(d.Id(), ":")
+	projectID := ids[0]
+	mirrorID := ids[1]
+	integerMirrorID, err := strconv.Atoi(mirrorID)
+	if err != nil {
+		return err
+	}
+	log.Printf("[DEBUG] read gitlab project mirror %s id %v", projectID, mirrorID)
+
+	mirrors, _, err := client.ProjectMirrors.ListProjectMirror(projectID)
+
+	if err != nil {
+		return err
+	}
+
+	var mirror *gitlab.ProjectMirror
+	found := false
+
+	for _, m := range mirrors {
+		log.Printf("[DEBUG] project mirror found %v", m.ID)
+		if m.ID == integerMirrorID {
+			mirror = m
+			found = true
+		}
+	}
+
+	if !found {
+		d.SetId("")
+		return nil
+	}
+
+	resourceGitlabProjectMirrorSetToState(d, mirror, &projectID)
+	return nil
+}
+
+func resourceGitlabProjectMirrorSetToState(d *schema.ResourceData, projectMirror *gitlab.ProjectMirror, projectID *string) {
+
+	mirrorID := strconv.Itoa(projectMirror.ID)
+	d.Set("enabled", projectMirror.Enabled)
+	d.Set("only_protected_branches", projectMirror.OnlyProtectedBranches)
+	d.Set("keep_divergent_refs", projectMirror.KeepDivergentRefs)
+	d.SetId(buildTwoPartID(projectID, &mirrorID))
+}
---
+++ gitlab/resource_gitlab_project_mirror_test.go
@@ -0,0 +1,213 @@
+package gitlab
+
+// TODO convert to handle project mirror.
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/xanzy/go-gitlab"
+)
+
+func TestAccGitlabProjectMirror_basic(t *testing.T) {
+	var hook gitlab.ProjectMirror
+	rInt := acctest.RandInt()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckGitlabProjectMirrorDestroy,
+		Steps: []resource.TestStep{
+			// Create a project and hook with default options
+			{
+				Config: testAccGitlabProjectMirrorConfig(rInt),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckGitlabProjectMirrorExists("gitlab_project_mirror.foo", &hook),
+					testAccCheckGitlabProjectMirrorAttributes(&hook, &testAccGitlabProjectMirrorExpectedAttributes{
+						URL:                   fmt.Sprintf("https://example.com/hook-%d", rInt),
+						Enabled:               true,
+						OnlyProtectedBranches: true,
+						KeepDivergentRefs:     true,
+					}),
+				),
+			},
+			// Update the project hook to toggle all the values to their inverse
+			{
+				Config: testAccGitlabProjectMirrorUpdateConfig(rInt),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckGitlabProjectMirrorExists("gitlab_project_mirror.foo", &hook),
+					testAccCheckGitlabProjectMirrorAttributes(&hook, &testAccGitlabProjectMirrorExpectedAttributes{
+						URL:                   fmt.Sprintf("https://example.com/hook-%d", rInt),
+						Enabled:               false,
+						OnlyProtectedBranches: false,
+						KeepDivergentRefs:     false,
+					}),
+				),
+			},
+			// Update the project hook to toggle the options back
+			{
+				Config: testAccGitlabProjectMirrorConfig(rInt),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckGitlabProjectMirrorExists("gitlab_project_mirror.foo", &hook),
+					testAccCheckGitlabProjectMirrorAttributes(&hook, &testAccGitlabProjectMirrorExpectedAttributes{
+						URL:                   fmt.Sprintf("https://example.com/hook-%d", rInt),
+						Enabled:               true,
+						OnlyProtectedBranches: true,
+						KeepDivergentRefs:     true,
+					}),
+				),
+			},
+		},
+	})
+}
+
+func TestAccGitlabProjectMirror_import(t *testing.T) {
+	rInt := acctest.RandInt()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckGitlabProjectMirrorDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccGitlabProjectMirrorConfig(rInt),
+			},
+			{
+				ResourceName:            "gitlab_project_mirror.foo",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"enabled", "mirror_id", "project", "url"},
+			},
+		},
+	})
+}
+
+func testAccCheckGitlabProjectMirrorExists(n string, mirror *gitlab.ProjectMirror) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not Found: %s", n)
+		}
+
+		splitID := strings.Split(rs.Primary.ID, ":")
+
+		mirrorID, err := strconv.Atoi(splitID[len(splitID)-1])
+		if err != nil {
+			return err
+		}
+		repoName := rs.Primary.Attributes["project"]
+		if repoName == "" {
+			return fmt.Errorf("No project ID is set")
+		}
+		conn := testAccProvider.Meta().(*gitlab.Client)
+
+		mirrors, _, err := conn.ProjectMirrors.ListProjectMirror(repoName, nil)
+		if err != nil {
+			return err
+		}
+
+		for _, m := range mirrors {
+			if m.ID == mirrorID {
+				*mirror = *m
+				break
+			}
+			return errors.New("unable to find mirror.")
+		}
+		return nil
+	}
+}
+
+type testAccGitlabProjectMirrorExpectedAttributes struct {
+	URL                   string
+	Enabled               bool
+	OnlyProtectedBranches bool
+	KeepDivergentRefs     bool
+}
+
+func testAccCheckGitlabProjectMirrorAttributes(mirror *gitlab.ProjectMirror, want *testAccGitlabProjectMirrorExpectedAttributes) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		if mirror.URL != want.URL {
+			return fmt.Errorf("got url %q; want %q", mirror.URL, want.URL)
+		}
+
+		if mirror.Enabled != want.Enabled {
+			return fmt.Errorf("got enable_ssl_verification %t; want %t", mirror.Enabled, want.Enabled)
+		}
+		if mirror.OnlyProtectedBranches != want.OnlyProtectedBranches {
+			return fmt.Errorf("got enable_ssl_verification %t; want %t", mirror.OnlyProtectedBranches, want.OnlyProtectedBranches)
+		}
+		if mirror.KeepDivergentRefs != want.KeepDivergentRefs {
+			return fmt.Errorf("got enable_ssl_verification %t; want %t", mirror.KeepDivergentRefs, want.KeepDivergentRefs)
+		}
+		return nil
+	}
+}
+
+func testAccCheckGitlabProjectMirrorDestroy(s *terraform.State) error {
+	conn := testAccProvider.Meta().(*gitlab.Client)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "gitlab_project" {
+			continue
+		}
+
+		gotRepo, resp, err := conn.Projects.GetProject(rs.Primary.ID, nil)
+		if err == nil {
+			if gotRepo != nil && fmt.Sprintf("%d", gotRepo.ID) == rs.Primary.ID {
+				if gotRepo.MarkedForDeletionAt == nil {
+					return fmt.Errorf("Repository still exists")
+				}
+			}
+		}
+		if resp.StatusCode != 404 {
+			return err
+		}
+		return nil
+	}
+	return nil
+}
+
+func testAccGitlabProjectMirrorConfig(rInt int) string {
+	return fmt.Sprintf(`
+resource "gitlab_project" "foo" {
+  name = "foo-%d"
+  description = "Terraform acceptance tests"
+
+  # So that acceptance tests can be run in a gitlab organization
+  # with no billing
+  visibility_level = "public"
+}
+
+resource "gitlab_project_mirror" "foo" {
+  project = "${gitlab_project.foo.id}"
+  url = "https://example.com/hook-%d"
+}
+	`, rInt, rInt)
+}
+
+func testAccGitlabProjectMirrorUpdateConfig(rInt int) string {
+	return fmt.Sprintf(`
+resource "gitlab_project" "foo" {
+  name = "foo-%d"
+  description = "Terraform acceptance tests"
+
+  # So that acceptance tests can be run in a gitlab organization
+  # with no billing
+  visibility_level = "public"
+}
+
+resource "gitlab_project_mirror" "foo" {
+  project = "${gitlab_project.foo.id}"
+  url = "https://example.com/hook-%d"
+  enabled = false
+  only_protected_branches = false
+  keep_divergent_refs = false
+}
+	`, rInt, rInt)
+}
