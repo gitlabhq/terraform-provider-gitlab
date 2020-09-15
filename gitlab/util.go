68c8c0e4cf14fda698bcacb74cb01fcfe7128815
Adam Snyder <armsnyder@gmail.com>
2020-09-14 18:44:27 -0700

    gitlab_project_variable: fix nondeterministic reads when using environment_scope (#409)
    * gitlab_project_variable: fix nondeterministic reads when using environment_scope
    
    * Respond to PR review
    - remove indirection to build and parse the resource id
    - link to issue in the resource docs
    - small test cleanup by making a isGitLabVersionLessThan SkipFunc

--- gitlab/util.go
+++ gitlab/util.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/url"
 	"regexp"
+	"strconv"
 	"strings"
 	"time"
 
@@ -216,4 +217,59 @@ func stringSetToStringSlice(stringSet *schema.Set) *[]string {
 		ret = append(ret, envVal.(string))
 	}
 	return &ret
+}
+
+// isGitLabVersionLessThan is a SkipFunc that returns true if the provided version is lower then
+// the current version of GitLab. It only checks the major and minor version numbers, not the patch.
+func isGitLabVersionLessThan(client *gitlab.Client, version string) func() (bool, error) {
+	return func() (bool, error) {
+		isAtLeast, err := isGitLabVersionAtLeast(client, version)()
+		return !isAtLeast, err
+	}
+}
+
+// isGitLabVersionAtLeast is a SkipFunc that checks that the version of GitLab is at least the
+// provided wantVersion. It only checks the major and minor version numbers, not the patch.
+func isGitLabVersionAtLeast(client *gitlab.Client, wantVersion string) func() (bool, error) {
+	return func() (bool, error) {
+		wantMajor, wantMinor, err := parseVersionMajorMinor(wantVersion)
+		if err != nil {
+			return false, fmt.Errorf("failed to parse wanted version %q: %w", wantVersion, err)
+		}
+
+		actualVersion, _, err := client.Version.GetVersion()
+		if err != nil {
+			return false, err
+		}
+
+		actualMajor, actualMinor, err := parseVersionMajorMinor(actualVersion.Version)
+		if err != nil {
+			return false, fmt.Errorf("failed to parse actual version %q: %w", actualVersion.Version, err)
+		}
+
+		if actualMajor == wantMajor {
+			return actualMinor >= wantMinor, nil
+		}
+
+		return actualMajor > wantMajor, nil
+	}
+}
+
+func parseVersionMajorMinor(version string) (int, int, error) {
+	parts := strings.SplitN(version, ".", 3)
+	if len(parts) < 2 {
+		return 0, 0, fmt.Errorf("need at least 2 parts (was %d)", len(parts))
+	}
+
+	major, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return 0, 0, err
+	}
+
+	minor, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return 0, 0, err
+	}
+
+	return major, minor, nil
 }
