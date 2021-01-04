1d2b03ba24a09bdb1202606c153eb67a076088b5
Zack K <zachk@knowbe4.com>
2021-01-04 11:36:18 -0500

    Added freeze period resource

---
+++ gitlab/resource_gitlab_project_freeze_period.go
@@ -0,0 +1,164 @@
+package gitlab
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	gitlab "github.com/xanzy/go-gitlab"
+)
+
+func resourceGitlabProjectFreezePeriod() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceGitlabProjectFreezePeriodCreate,
+		Read:   resourceGitlabProjectApprovalRuleRead,
+		Update: resourceGitlabProjectApprovalRuleUpdate,
+		Delete: resourceGitlabProjectApprovalRuleDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema: map[string]*schema.Schema{
+			"project": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"freeze_start": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"freeze_end": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"cron_timezone": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "UTC",
+			},
+		},
+	}
+}
+
+func resourceGitlabProjectFreezePeriodCreate(d *schema.ResourceData, meta interface{}) error {
+	project := d.Get("project").(string)
+
+	options := gitlab.CreateFreezePeriodOptions{
+		FreezeStart:  gitlab.String(d.Get("freeze_start").(string)),
+		FreezeEnd:    gitlab.String(d.Get("freeze_end").(string)),
+		CronTimezone: gitlab.String(d.Get("cron_timezone").(string)),
+	}
+
+	log.Printf("[DEBUG] Project %s create gitlab project-level freeze period %+v", project, options)
+
+	client := meta.(*gitlab.Client)
+	FreezePeriod, _, err := client.FreezePeriod.CreateFreezePeriodOptions(project, &options)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(strconv.Itoa(FreezePeriod.ID))
+
+	return resourceGitlabProjectFreezePeriodRead(d, meta)
+}
+
+func resourceGitlabProjectFreezePeriodRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*gitlab.Client)
+	project := d.Get("project").(string)
+	freezePeriodID, err := strconv.Atoi(d.Id())
+
+	if err != nil {
+		return fmt.Errorf("%s cannot be converted to int", d.Id())
+	}
+
+	log.Printf("[DEBUG] read gitlab FreezePeriod %s/%d", project, freezePeriodID)
+
+	opt := &gitlab.ListFreezePeriodsOptions{
+		Page:    1,
+		PerPage: 20,
+	}
+
+	found := false
+	for {
+		freezePeriods, resp, err := client.FreezePeriod.ListFreezePeriods(project, opt)
+		if err != nil {
+			return err
+		}
+		for _, freezePeriod := range freezePeriods {
+			if freezePeriod.ID == freezePeriodID {
+				d.Set("id", freezePeriod.ID)
+				d.Set("freeze_start", freezePeriod.FreezeStart)
+				d.Set("freeze_end", freezePeriod.FreezeEnd)
+				d.Set("cron_timezone", freezePeriod.CronTimezone)
+				found = true
+				break
+			}
+		}
+
+		if found || resp.CurrentPage >= resp.TotalPages {
+			break
+		}
+
+		opt.Page = resp.NextPage
+	}
+	if !found {
+		return fmt.Errorf("FreezePeriod %d no longer exists in gitlab", freezePeriodID)
+	}
+
+	return nil
+}
+
+func resourceGitlabProjectFreezePeriodUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*gitlab.Client)
+	project := d.Get("project").(string)
+	options := &gitlab.UpdateFreezePeriodOptions{
+		FreezeStart:  gitlab.String(d.Get("freeze_start").(string)),
+		FreezeEnd:    gitlab.String(d.Get("freeze_end").(string)),
+		CronTimezone: gitlab.String(d.Get("cron_timezone").(string)),
+	}
+
+	freezePeriodID, err := strconv.Atoi(d.Id())
+
+	if err != nil {
+		return fmt.Errorf("%s cannot be converted to int", d.Id())
+	}
+
+	if d.HasChange("freeze_start") {
+		options.FreezeStart = gitlab.String(d.Get("freeze_start").(string))
+	}
+
+	if d.HasChange("freeze_end") {
+		options.FreezeEnd = gitlab.String(d.Get("freeze_end").(string))
+	}
+
+	if d.HasChange("cron_timezone") {
+		options.CronTimezone = gitlab.String(d.Get("cron_timezone").(string))
+	}
+
+	log.Printf("[DEBUG] update gitlab FreezePeriod %s", d.Id())
+
+	_, _, err = client.FreezePeriod.UpdateFreezePeriodOptions(project, freezePeriodID, options)
+	if err != nil {
+		return err
+	}
+
+	return resourceGitlabProjectFreezePeriodRead(d, meta)
+}
+
+func resourceGitlabProjectFreezePeriodDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*gitlab.Client)
+	project := d.Get("project").(string)
+	log.Printf("[DEBUG] Delete gitlab FreezePeriod %s", d.Id())
+
+	FreezePeriodID, err := strconv.Atoi(d.Id())
+
+	if err != nil {
+		return fmt.Errorf("%s cannot be converted to int", d.Id())
+	}
+
+	if _, err = client.FreezePeriod.DeleteFreezePeriod(project, FreezePeriodID); err != nil {
+		return fmt.Errorf("failed to delete pipeline schedule %q: %w", d.Id(), err)
+	}
+
+	return nil
+}
---
+++ gitlab/resource_gitlab_project_freeze_period_test.go
@@ -0,0 +1,176 @@
+package gitlab
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	gitlab "github.com/xanzy/go-gitlab"
+)
+
+func TestAccGitlabFreezePeriod_basic(t *testing.T) {
+	var schedule gitlab.FreezePeriod
+	rInt := acctest.RandInt()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckGitlabFreezePeriodDestroy,
+		Steps: []resource.TestStep{
+			// Create a project and freeze period with default options
+			{
+				Config: testAccGitlabFreezePeriodConfig(rInt),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckGitlabFreezePeriodExists("gitlab_project_freeze_period.schedule", &schedule),
+					testAccCheckGitlabFreezePeriodAttributes(&schedule, &testAccGitlabFreezePeriodExpectedAttributes{
+						FreezeStart:  "0 23 * * 5",
+						FreezeEnd:    "0 7 * * 1",
+						CronTimezone: "UTC",
+					}),
+				),
+			},
+			// Update the freeze period to change the parameters
+			{
+				Config: testAccGitlabFreezePeriodUpdateConfig(rInt),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckGitlabFreezePeriodExists("gitlab_project_freeze_period.schedule", &schedule),
+					testAccCheckGitlabFreezePeriodAttributes(&schedule, &testAccGitlabFreezePeriodExpectedAttributes{
+						FreezeStart:  "0 23 * * 5",
+						FreezeEnd:    "0 7 * * 3",
+						CronTimezone: "UTC",
+					}),
+				),
+			},
+			// Update the freeze period to get back to initial settings
+			{
+				Config: testAccGitlabFreezePeriodConfig(rInt),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckGitlabFreezePeriodExists("gitlab_project_freeze_period.schedule", &schedule),
+					testAccCheckGitlabFreezePeriodAttributes(&schedule, &testAccGitlabFreezePeriodExpectedAttributes{
+						FreezeStart:  "0 23 * * 5",
+						FreezeEnd:    "0 7 * * 1",
+						CronTimezone: "UTC",
+					}),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckGitlabFreezePeriodExists(n string, freezePeriod *gitlab.FreezePeriod) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not Found: %s", n)
+		}
+
+		scheduleID := rs.Primary.ID
+		repoName := rs.Primary.Attributes["project"]
+		if repoName == "" {
+			return fmt.Errorf("No project ID is set")
+		}
+		conn := testAccProvider.Meta().(*gitlab.Client)
+
+		freezePeriods, _, err := conn.FreezePeriod.ListFreezePeriods(repoName, nil)
+		if err != nil {
+			return err
+		}
+		for _, gotFreezePeriod := range freezePeriods {
+			if strconv.Itoa(gotFreezePeriod.ID) == scheduleID {
+				*freezePeriod = *gotFreezePeriod
+				return nil
+			}
+		}
+		return fmt.Errorf("Freeze Period does not exist")
+	}
+}
+
+type testAccGitlabFreezePeriodExpectedAttributes struct {
+	FreezeStart  string
+	FreezeEnd    string
+	CronTimezone string
+}
+
+func testAccCheckGitlabFreezePeriodAttributes(freezePeriod *gitlab.FreezePeriod, want *testAccGitlabFreezePeriodExpectedAttributes) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		if freezePeriod.FreezeStart != want.FreezeStart {
+			return fmt.Errorf("got freeze_start %q; want %q", freezePeriod.FreezeStart, want.FreezeStart)
+		}
+		if freezePeriod.FreezeEnd != want.FreezeEnd {
+			return fmt.Errorf("got freeze_end %q; want %q", freezePeriod.FreezeEnd, want.FreezeEnd)
+		}
+
+		if freezePeriod.CronTimezone != want.CronTimezone {
+			return fmt.Errorf("got cron_timezone %q; want %q", freezePeriod.CronTimezone, want.CronTimezone)
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckGitlabFreezePeriodDestroy(s *terraform.State) error {
+	conn := testAccProvider.Meta().(*gitlab.Client)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "gitlab_project" {
+			continue
+		}
+
+		gotRepo, resp, err := conn.Projects.GetProject(rs.Primary.ID, nil)
+		if err == nil {
+			if gotRepo != nil && fmt.Sprintf("%d", gotRepo.ID) == rs.Primary.ID {
+				if gotRepo.MarkedForDeletionAt == nil {
+					return fmt.Errorf("Repository still exists")
+				}
+			}
+		}
+		if resp.StatusCode != 404 {
+			return err
+		}
+		return nil
+	}
+	return nil
+}
+
+func testAccGitlabFreezePeriodConfig(rInt int) string {
+	return fmt.Sprintf(`
+resource "gitlab_project" "foo" {
+  name = "foo-%d"
+  description = "Terraform acceptance tests"
+
+  # So that acceptance tests can be run in a gitlab organization
+  # with no billing
+  visibility_level = "public"
+}
+
+resource "gitlab_project_freeze_period" "schedule" {
+	project = "${gitlab_project.foo.id}"
+	freeze_start = "0 23 * * 5"
+	freeze_end =  "0 7 * * 1"
+	cron_timezone = "UTC"
+}
+	`, rInt)
+}
+
+func testAccGitlabFreezePeriodUpdateConfig(rInt int) string {
+	return fmt.Sprintf(`
+resource "gitlab_project" "foo" {
+  name = "foo-%d"
+  description = "Terraform acceptance tests"
+
+  # So that acceptance tests can be run in a gitlab organization
+  # with no billing
+  visibility_level = "public"
+}
+
+resource "gitlab_project_freeze_period" "schedule" {
+  project = "${gitlab_project.foo.id}"
+  freeze_start = "0 23 * * 5"
+  freeze_end =  "0 7 * * 3"
+  cron_timezone = "UTC"
+}
+	`, rInt)
+}
