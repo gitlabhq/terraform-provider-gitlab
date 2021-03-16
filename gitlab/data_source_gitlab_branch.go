bd9c85361dabed7d93e623fd48d72c1b329d09aa
eddb7 <50178783+eddb7@users.noreply.github.com>
2021-03-16 19:56:17 +0000

    Update data_source_gitlab_branch.go
    remove unused import

--- gitlab/data_source_gitlab_branch.go
+++ gitlab/data_source_gitlab_branch.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 
-	// "github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/xanzy/go-gitlab"
 )
