88f2ea8c622438f4c5519faec26b63260d4af11d
Rene Zbinden <rene.zbinden@postfinance.ch>
2021-07-23 17:44:32 +0200

    add support for gitlab group badges

--- gitlab/resource_gitlab_group_badge.go
+++ gitlab/resource_gitlab_group_badge.go
@@ -1,61 +1,50 @@
 package gitlab
 
 import (
-	"context"
 	"log"
 	"strconv"
 	"strings"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	gitlab "github.com/xanzy/go-gitlab"
 )
 
 func resourceGitlabGroupBadge() *schema.Resource {
 	return &schema.Resource{
-		Description: "This resource allows you to create and manage badges for your GitLab groups.\n" +
-			"For further information, consult the [gitlab\n" +
-			"documentation](https://docs.gitlab.com/ee/user/project/badges.html#group-badges).",
-
-		CreateContext: resourceGitlabGroupBadgeCreate,
-		ReadContext:   resourceGitlabGroupBadgeRead,
-		UpdateContext: resourceGitlabGroupBadgeUpdate,
-		DeleteContext: resourceGitlabGroupBadgeDelete,
+		Create: resourceGitlabGroupBadgeCreate,
+		Read:   resourceGitlabGroupBadgeRead,
+		Update: resourceGitlabGroupBadgeUpdate,
+		Delete: resourceGitlabGroupBadgeDelete,
 		Importer: &schema.ResourceImporter{
-			StateContext: schema.ImportStatePassthroughContext,
+			State: schema.ImportStatePassthrough,
 		},
 
 		Schema: map[string]*schema.Schema{
 			"group": {
-				Description: "The id of the group to add the badge to.",
-				Type:        schema.TypeString,
-				Required:    true,
+				Type:     schema.TypeString,
+				Required: true,
 			},
 			"link_url": {
-				Description: "The url linked with the badge.",
-				Type:        schema.TypeString,
-				Required:    true,
+				Type:     schema.TypeString,
+				Required: true,
 			},
 			"image_url": {
-				Description: "The image url which will be presented on group overview.",
-				Type:        schema.TypeString,
-				Required:    true,
+				Type:     schema.TypeString,
+				Required: true,
 			},
 			"rendered_link_url": {
-				Description: "The link_url argument rendered (in case of use of placeholders).",
-				Type:        schema.TypeString,
-				Computed:    true,
+				Type:     schema.TypeString,
+				Computed: true,
 			},
 			"rendered_image_url": {
-				Description: "The image_url argument rendered (in case of use of placeholders).",
-				Type:        schema.TypeString,
-				Computed:    true,
+				Type:     schema.TypeString,
+				Computed: true,
 			},
 		},
 	}
 }
 
-func resourceGitlabGroupBadgeCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+func resourceGitlabGroupBadgeCreate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*gitlab.Client)
 	groupID := d.Get("group").(string)
 	options := &gitlab.AddGroupBadgeOptions{
@@ -65,45 +54,45 @@ func resourceGitlabGroupBadgeCreate(ctx context.Context, d *schema.ResourceData,
 
 	log.Printf("[DEBUG] create gitlab group variable %s/%s", *options.LinkURL, *options.ImageURL)
 
-	badge, _, err := client.GroupBadges.AddGroupBadge(groupID, options, gitlab.WithContext(ctx))
+	badge, _, err := client.GroupBadges.AddGroupBadge(groupID, options)
 	if err != nil {
-		return diag.FromErr(err)
+		return err
 	}
 
 	badgeID := strconv.Itoa(badge.ID)
 
 	d.SetId(buildTwoPartID(&groupID, &badgeID))
 
-	return resourceGitlabGroupBadgeRead(ctx, d, meta)
+	return resourceGitlabGroupBadgeRead(d, meta)
 }
 
-func resourceGitlabGroupBadgeRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+func resourceGitlabGroupBadgeRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*gitlab.Client)
 	ids := strings.Split(d.Id(), ":")
 	groupID := ids[0]
 	badgeID, err := strconv.Atoi(ids[1])
 	if err != nil {
-		return diag.FromErr(err)
+		return err
 	}
 
 	log.Printf("[DEBUG] read gitlab group badge %s/%d", groupID, badgeID)
 
-	badge, _, err := client.GroupBadges.GetGroupBadge(groupID, badgeID, gitlab.WithContext(ctx))
+	badge, _, err := client.GroupBadges.GetGroupBadge(groupID, badgeID)
 	if err != nil {
-		return diag.FromErr(err)
+		return err
 	}
 
 	resourceGitlabGroupBadgeSetToState(d, badge, &groupID)
 	return nil
 }
 
-func resourceGitlabGroupBadgeUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+func resourceGitlabGroupBadgeUpdate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*gitlab.Client)
 	ids := strings.Split(d.Id(), ":")
 	groupID := ids[0]
 	badgeID, err := strconv.Atoi(ids[1])
 	if err != nil {
-		return diag.FromErr(err)
+		return err
 	}
 
 	options := &gitlab.EditGroupBadgeOptions{
@@ -113,31 +102,27 @@ func resourceGitlabGroupBadgeUpdate(ctx context.Context, d *schema.ResourceData,
 
 	log.Printf("[DEBUG] update gitlab group badge %s/%d", groupID, badgeID)
 
-	_, _, err = client.GroupBadges.EditGroupBadge(groupID, badgeID, options, gitlab.WithContext(ctx))
+	_, _, err = client.GroupBadges.EditGroupBadge(groupID, badgeID, options)
 	if err != nil {
-		return diag.FromErr(err)
+		return err
 	}
 
-	return resourceGitlabGroupBadgeRead(ctx, d, meta)
+	return resourceGitlabGroupBadgeRead(d, meta)
 }
 
-func resourceGitlabGroupBadgeDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+func resourceGitlabGroupBadgeDelete(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*gitlab.Client)
 	ids := strings.Split(d.Id(), ":")
 	groupID := ids[0]
 	badgeID, err := strconv.Atoi(ids[1])
 	if err != nil {
-		return diag.FromErr(err)
+		return err
 	}
 
 	log.Printf("[DEBUG] Delete gitlab group badge %s/%d", groupID, badgeID)
 
-	_, err = client.GroupBadges.DeleteGroupBadge(groupID, badgeID, gitlab.WithContext(ctx))
-	if err != nil {
-		return diag.FromErr(err)
-	}
-
-	return nil
+	_, err = client.GroupBadges.DeleteGroupBadge(groupID, badgeID)
+	return err
 }
 
 func resourceGitlabGroupBadgeSetToState(d *schema.ResourceData, badge *gitlab.GroupBadge, groupID *string) {
