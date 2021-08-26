cde640b7ee98291ea9f6f93a137738ee1471f3a4
Ed B <edward.butler.external@atosbpstestworkplace.onmicrosoft.com>
2021-08-26 16:08:19 +0100

    added importer

--- gitlab/resource_gitlab_branch.go
+++ gitlab/resource_gitlab_branch.go
@@ -11,6 +11,9 @@ func resourceGitlabBranch() *schema.Resource {
 		Create: resourceGitlabBranchCreate,
 		Read:   resourceGitlabBranchRead,
 		Delete: resourceGitlabBranchDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
 		Schema: map[string]*schema.Schema{
 			"name": {
 				Type:     schema.TypeString,
