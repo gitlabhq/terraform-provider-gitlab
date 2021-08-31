d4f906519effdf663838872a829fae7cac466d4f
Ed B <edward.butler.external@atosbpstestworkplace.onmicrosoft.com>
2021-08-31 17:49:04 +0100

    update to working test

--- gitlab/resource_gitlab_branch_test.go
+++ gitlab/resource_gitlab_branch_test.go
@@ -47,12 +47,11 @@ func TestAccGitlabBranch_basic(t *testing.T) {
 func testAccCheckGitlabBranchCommit(n string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs := s.RootModule().Resources[fmt.Sprintf("gitlab_branch.%s", n)]
-		attributes := rs.Primary.Attributes
-		commit := rs.Primary.Attributes["commit"]
-		// if commit == "" {
-		// 	return fmt.Errorf("expected commit to be populated")
-		// }
-		return fmt.Errorf("%+v \n %+v", commit, attributes)
+		commit := rs.Primary.Attributes["commit.0.id"]
+		if commit == "" {
+			return fmt.Errorf("expected commit to be populated")
+		}
+		return nil
 	}
 }
 
