5b5b97f1fd0141aae7f693b5da00e58def7d3382
Ed B <edward.butler.external@atosbpstestworkplace.onmicrosoft.com>
2021-08-31 17:05:46 +0100

    force error

--- gitlab/resource_gitlab_branch_test.go
+++ gitlab/resource_gitlab_branch_test.go
@@ -47,11 +47,11 @@ func TestAccGitlabBranch_basic(t *testing.T) {
 func testAccCheckGitlabBranchCommit(n string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs := s.RootModule().Resources[fmt.Sprintf("gitlab_branch.%s", n)]
-		commit := rs.Primary.Attributes["commit.id"]
-		if commit == "" {
-			return fmt.Errorf("expected commit to be populated")
-		}
-		return nil
+		commit := rs.Primary.Attributes["commit"]
+		// if commit == "" {
+		// 	return fmt.Errorf("expected commit to be populated")
+		// }
+		return fmt.Errorf("%+v", commit)
 	}
 }
 
