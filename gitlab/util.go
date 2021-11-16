42abef0a242d11e8f70962046e25d75888a77a39
Koen Rutten <13603490+KoenR3@users.noreply.github.com>
2021-11-16 11:45:31 +0100

    Add minimal permissions

--- gitlab/util.go
+++ gitlab/util.go
@@ -14,6 +14,7 @@ import (
 
 var accessLevelNameToValue = map[string]gitlab.AccessLevelValue{
 	"no one":     gitlab.NoPermissions,
+	"minimal":    gitlab.MinimalAccessPermissions,
 	"guest":      gitlab.GuestPermissions,
 	"reporter":   gitlab.ReporterPermissions,
 	"developer":  gitlab.DeveloperPermissions,
@@ -25,12 +26,13 @@ var accessLevelNameToValue = map[string]gitlab.AccessLevelValue{
 }
 
 var accessLevelValueToName = map[gitlab.AccessLevelValue]string{
-	gitlab.NoPermissions:         "no one",
-	gitlab.GuestPermissions:      "guest",
-	gitlab.ReporterPermissions:   "reporter",
-	gitlab.DeveloperPermissions:  "developer",
-	gitlab.MaintainerPermissions: "maintainer",
-	gitlab.OwnerPermissions:      "owner",
+	gitlab.NoPermissions:         		"no one",
+	gitlab.MinimalAccessPermissions: 	"minimal",
+	gitlab.GuestPermissions:      		"guest",
+	gitlab.ReporterPermissions:   		"reporter",
+	gitlab.DeveloperPermissions:  		"developer",
+	gitlab.MaintainerPermissions: 		"maintainer",
+	gitlab.OwnerPermissions:      		"owner",
 }
 
 // copied from ../github/util.go
