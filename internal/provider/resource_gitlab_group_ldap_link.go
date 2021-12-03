3545901e92f2a22e8ba3ad9e365b9c8301c06003
Han Zhou <han.zhou@booking.com>
2021-12-03 23:52:07 +0100

    fix(group_ldap_link): add missing terraform import feature for group LDAP link **Why**
    There is a bug that [the doc](https://registry.terraform.io/providers/gitlabhq/gitlab/latest/docs/resources/group_ldap_link#import) says we can use `terraform import`, but actually the function is not implemented
    
    **Code Changes**
    
    * Added the missing import function
    * Fixed a bug to set `access_level` instead of `group_level`, because `access_level` is the one defined in schema
    * Set `force = false` as default when importing, because we can not get any force value from the Gitlab API
    * Updated the doc about import part
    
    **Design Changes**
    
    * Added `group_id` in the import id string to change it from `ldap_provider:cn` to  `group_id:ldap_provider:cn`, because we need the `group_id` to identify the group
    
    **Close**
    
    * https://github.com/gitlabhq/terraform-provider-gitlab/issues/448

--- internal/provider/resource_gitlab_group_ldap_link.go
+++ internal/provider/resource_gitlab_group_ldap_link.go
@@ -20,6 +20,9 @@ var _ = registerResource("gitlab_group_ldap_link", func() *schema.Resource {
 		CreateContext: resourceGitlabGroupLdapLinkCreate,
 		ReadContext:   resourceGitlabGroupLdapLinkRead,
 		DeleteContext: resourceGitlabGroupLdapLinkDelete,
+		Importer: &schema.ResourceImporter{
+			State: resourceGitlabGroupLdapLinkImporter,
+		},
 
 		Schema: map[string]*schema.Schema{
 			"group_id": {
@@ -179,4 +182,19 @@ func resourceGitlabGroupLdapLinkDelete(ctx context.Context, d *schema.ResourceDa
 	}
 
 	return nil
+}
+
+func resourceGitlabGroupLdapLinkImporter(ctx context.Context, d *schema.ResourceData, meta interface{}) ([]*schema.ResourceData, diag.Diagnostics) {
+	parts := strings.SplitN(d.Id(), ":", 3)
+	if len(parts) != 3 {
+		return nil, diag.FromErr(fmt.Errorf("invalid ldap link import id (should be <group id>:<ldap provider>:<ladp cn>): %s", d.Id()))
+	}
+
+	groupId, ldapProvider, ldapCN := parts[0], parts[1], parts[2]
+	d.SetId(buildTwoPartID(&ldapProvider, &ldapCN))
+	d.Set("group_id", groupId)
+	d.Set("force", false)
+
+	diag := resourceGitlabGroupLdapLinkRead(ctx, d, meta)
+	return []*schema.ResourceData{d}, diag
 }
