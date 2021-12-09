708ae15670e82d7d020279b855ceaa094c4ebebb
Elio Bischof <eliobischof@gmail.com>
2021-12-09 13:40:27 +0100

    make topic tests pass

--- internal/provider/resource_gitlab_topic_test.go
+++ internal/provider/resource_gitlab_topic_test.go
@@ -1,17 +1,17 @@
 package provider
 
-/*
 import (
 	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 	"github.com/xanzy/go-gitlab"
 )
 
-func TestAccGitlabTopic_basic(t *testing.T) {
+func TestAccGitlabTopic(t *testing.T) {
 	var topic gitlab.Topic
 	rInt := acctest.RandInt()
 
@@ -22,37 +22,32 @@ func TestAccGitlabTopic_basic(t *testing.T) {
 		Steps: []resource.TestStep{
 			// Create a topic with default options
 			{
-				Config: testAccGitlabTopicCreateConfig(rInt),
+				Config: testAccGitlabTopicRequiredConfig(rInt),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckGitlabTopicExists("gitlab_group_label.fixme", &topic),
+					testAccCheckGitlabTopicExists("gitlab_topic.foo", &topic),
 					testAccCheckGitlabTopicAttributes(&topic, &testAccGitlabTopicExpectedAttributes{
-						Name:        fmt.Sprintf("FIXME-%d", rInt),
-						Color:       "#ffcc00",
-						Description: "fix this test",
+						Name: fmt.Sprintf("foo-req-%d", rInt),
 					}),
 				),
 			},
 			// Update the topics values
 			{
-				Config: testAccGitlabTopicUpdateConfig(rInt),
+				Config: testAccGitlabTopicFullConfig(rInt),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckGitlabTopicExists("gitlab_group_label.fixme", &topic),
+					testAccCheckGitlabTopicExists("gitlab_topic.foo", &topic),
 					testAccCheckGitlabTopicAttributes(&topic, &testAccGitlabTopicExpectedAttributes{
-						Name:        fmt.Sprintf("FIXME-%d", rInt),
-						Color:       "#ff0000",
-						Description: "red label",
+						Name:        fmt.Sprintf("foo-full-%d", rInt),
+						Description: "Terraform acceptance tests",
 					}),
 				),
 			},
-			// Update the topic back to its initial state
+			// Update the topics values back to their initial state
 			{
-				Config: testAccGitlabTopicCreateConfig(rInt),
+				Config: testAccGitlabTopicRequiredConfig(rInt),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckGitlabTopicExists("gitlab_group_label.fixme", &topic),
+					testAccCheckGitlabTopicExists("gitlab_topic.foo", &topic),
 					testAccCheckGitlabTopicAttributes(&topic, &testAccGitlabTopicExpectedAttributes{
-						Name:        fmt.Sprintf("FIXME-%d", rInt),
-						Color:       "#ff0000",
-						Description: "red label",
+						Name: fmt.Sprintf("foo-req-%d", rInt),
 					}),
 				),
 			},
@@ -60,95 +55,36 @@ func TestAccGitlabTopic_basic(t *testing.T) {
 	})
 }
 
-// lintignore: AT002 // TODO: Resolve this tfproviderlint issue
-func TestAccGitlabTopic_import(t *testing.T) {
-	rInt := acctest.RandInt()
-	resourceName := "gitlab_group_label.fixme"
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckGitlabTopicDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccGitlabTopicConfig(rInt),
-			},
-			{
-				ResourceName:      resourceName,
-				ImportStateIdFunc: getTopicImportID(resourceName),
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
-		},
-	})
-}
-
-func getTopicImportID(n string) resource.ImportStateIdFunc {
-	return func(s *terraform.State) (string, error) {
-		rs, ok := s.RootModule().Resources[n]
-		if !ok {
-			return "", fmt.Errorf("Not Found: %s", n)
-		}
-
-		labelID := rs.Primary.ID
-		if labelID == "" {
-			return "", fmt.Errorf("No deploy key ID is set")
-		}
-		groupID := rs.Primary.Attributes["group"]
-		if groupID == "" {
-			return "", fmt.Errorf("No group ID is set")
-		}
-
-		return fmt.Sprintf("%s:%s", groupID, labelID), nil
-	}
-}
-
-func testAccCheckGitlabTopicExists(n string, label *gitlab.Topic) resource.TestCheckFunc {
+func testAccCheckGitlabTopicExists(n string, assign *gitlab.Topic) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[n]
 		if !ok {
-			return fmt.Errorf("Not Found: %s", n)
+			return fmt.Errorf("not Found: %s", n)
 		}
 
-		labelName := rs.Primary.ID
-		groupName := rs.Primary.Attributes["group"]
-		if groupName == "" {
-			return fmt.Errorf("No group ID is set")
-		}
+		topicID := rs.Primary.ID
 		conn := testAccProvider.Meta().(*gitlab.Client)
 
-		labels, _, err := conn.Topics.ListTopics(groupName, &gitlab.ListTopicsOptions{PerPage: 1000})
-		if err != nil {
-			return err
-		}
-		for _, gotLabel := range labels {
-			if gotLabel.Name == labelName {
-				*label = *gotLabel
-				return nil
-			}
-		}
-		return fmt.Errorf("Label does not exist")
+		topic, _, err := conn.Topics.GetTopic(topicID)
+		*assign = *topic
+
+		return err
 	}
 }
 
 type testAccGitlabTopicExpectedAttributes struct {
 	Name        string
-	Color       string
 	Description string
 }
 
-func testAccCheckGitlabTopicAttributes(label *gitlab.Topic, want *testAccGitlabTopicExpectedAttributes) resource.TestCheckFunc {
+func testAccCheckGitlabTopicAttributes(topic *gitlab.Topic, want *testAccGitlabTopicExpectedAttributes) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
-		if label.Name != want.Name {
-			return fmt.Errorf("got name %q; want %q", label.Name, want.Name)
+		if topic.Name != want.Name {
+			return fmt.Errorf("got name %q; want %q", topic.Name, want.Name)
 		}
 
-		if label.Description != want.Description {
-			return fmt.Errorf("got description %q; want %q", label.Description, want.Description)
-		}
-
-		if label.Color != want.Color {
-			return fmt.Errorf("got color %q; want %q", label.Color, want.Color)
+		if topic.Description != want.Description {
+			return fmt.Errorf("got description %q; want %q", topic.Description, want.Description)
 		}
 
 		return nil
@@ -159,16 +95,16 @@ func testAccCheckGitlabTopicDestroy(s *terraform.State) error {
 	conn := testAccProvider.Meta().(*gitlab.Client)
 
 	for _, rs := range s.RootModule().Resources {
-		if rs.Type != "gitlab_group" {
+		if rs.Type != "gitlab_topic" {
 			continue
 		}
 
-		group, resp, err := conn.Groups.GetGroup(rs.Primary.ID, nil)
+		topic, resp, err := conn.Topics.GetTopic(rs.Primary.ID)
 		if err == nil {
-			if group != nil && fmt.Sprintf("%d", group.ID) == rs.Primary.ID {
-				if group.MarkedForDeletionOn == nil {
-					return fmt.Errorf("Group still exists")
-				}
+			if topic != nil && fmt.Sprintf("%d", topic.ID) == rs.Primary.ID {
+				// TODO: Return error as soon as deleting a topic is supported
+				return nil
+				//				return fmt.Errorf("topic still exists")
 			}
 		}
 		if resp.StatusCode != 404 {
@@ -179,25 +115,17 @@ func testAccCheckGitlabTopicDestroy(s *terraform.State) error {
 	return nil
 }
 
-func testAccGitlabTopicCreateConfig(rInt int) string {
+func testAccGitlabTopicRequiredConfig(rInt int) string {
 	return fmt.Sprintf(`
 resource "gitlab_topic" "foo" {
-  name             = "foo-%d"
-  path             = "foo-%d"
-  description      = "Terraform acceptance tests"
-  visibility_level = "public"
-}
-	`, rInt)
+  name             = "foo-req-%d"
+}`, rInt)
 }
 
-func testAccGitlabTopicUpdateConfig(rInt int) string {
+func testAccGitlabTopicFullConfig(rInt int) string {
 	return fmt.Sprintf(`
 resource "gitlab_topic" "foo" {
-  name             = "foo-%d"
-  path             = "foo-%d"
+  name             = "foo-full-%d"
   description      = "Terraform acceptance tests"
-  visibility_level = "public"
+}`, rInt)
 }
-	`, rInt)
-}
-*/
