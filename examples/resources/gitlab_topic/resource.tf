6684f2a33b3eb08f50eae9191397f668bffb3dd5
Elio Bischof <eliobischof@gmail.com>
2022-02-01 16:42:25 +0100

    generate docs

--- examples/resources/gitlab_topic/resource.tf
+++ examples/resources/gitlab_topic/resource.tf
@@ -1,4 +1,4 @@
 resource "gitlab_topic" "functional-programming" {
-  name             = "Functional Programming"
-  description      = "In computer science, functional programming is a programming paradigm where programs are constructed by applying and composing functions."
+  name        = "Functional Programming"
+  description = "In computer science, functional programming is a programming paradigm where programs are constructed by applying and composing functions."
 }
