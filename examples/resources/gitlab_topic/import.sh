666bec91426ff782160aed143551f2c774b5472f
Elio Bischof <eliobischof@gmail.com>
2022-02-14 14:43:43 +0100

    import using the topic id

--- examples/resources/gitlab_topic/import.sh
+++ examples/resources/gitlab_topic/import.sh
@@ -1,3 +1,4 @@
-# You can import a topic to terraform state using `terraform import <resource>`.
+# You can import a topic to terraform state using `terraform import <resource> <id>`.
+# The `id` must be an integer for the id of the topic you want to import,
 # for example:
-terraform terraform import gitlab_topic.functional-programming
+terraform terraform import gitlab_topic.functional-programming 1
