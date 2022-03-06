4b962200a4b8a97b10893a9bc293d968d5547b94
Timo Furrer <tuxtimo@gmail.com>
2022-03-06 10:42:49 +0100

    New Datasource `gitlab_project_issue`
    Refs #746

---
+++ internal/provider/data_source_gitlab_project_issue.go
@@ -0,0 +1,38 @@
+package provider
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/xanzy/go-gitlab"
+)
+
+var _ = registerDataSource("gitlab_project_issue", func() *schema.Resource {
+	return &schema.Resource{
+		Description: `This data source allows you to access GitLab Project Issues.
+
+**Upstream API:** [GitLab API docs](https://docs.gitlab.com/ee/api/issues.html)`,
+
+		ReadContext: dataSourceGitlabProjectIssueRead,
+		Schema:      datasourceSchemaFromResourceSchema(gitlabProjectIssueGetSchema(), "project", "iid"),
+	}
+})
+
+func dataSourceGitlabProjectIssueRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*gitlab.Client)
+	project := d.Get("project").(string)
+	issueIID := d.Get("iid").(int)
+
+	issue, _, err := client.Issues.GetIssue(project, issueIID, gitlab.WithContext(ctx))
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	d.SetId(resourceGitLabProjectIssueBuildId(project, issueIID))
+	stateMap := gitlabProjectIssueToStateMap(project, issue)
+
+	if err := setStateMapInResourceData(stateMap, d); err != nil {
+		return diag.FromErr(err)
+	}
+	return nil
+}
