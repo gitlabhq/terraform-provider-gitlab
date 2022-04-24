578411f38252d8471f8bce8b78a05fb963d2d332
Viktor Nagy <viktor.nagy@gmail.com>
2022-04-24 18:01:39 +0000

    Set up pages again

--- scripts/gitlab.rb
+++ scripts/gitlab.rb
@@ -1,3 +1,8 @@
+pages_external_url 'http://127.0.0.1:5051'
+pages_nginx['redirect_http_to_https'] = false
+pages_nginx['ssl_certificate'] = "/etc/gitlab/ssl/gitlab-registry.pem"
+pages_nginx['ssl_certificate_key'] = "/etc/gitlab/ssl/gitlab-registry.key"
+
 registry_external_url 'http://127.0.0.1:5050'
 registry['enable']                    = true
 registry_nginx['ssl_certificate']     = "/etc/gitlab/ssl/gitlab-registry.pem"
