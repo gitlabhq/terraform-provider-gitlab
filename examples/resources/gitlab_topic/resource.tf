76b61f61e63e6a5b473573ccdab551158341fcbb
Timo Furrer <tuxtimo@gmail.com>
2022-04-30 20:16:20 +0200

    resource/gitlab_topic: Add `title` argument support for GitLab 15.0
    Closes: #1043

--- examples/resources/gitlab_topic/resource.tf
+++ examples/resources/gitlab_topic/resource.tf
@@ -1,5 +1,6 @@
 resource "gitlab_topic" "functional_programming" {
-  name        = "Functional Programming"
+  name        = "functional-programming"
+  title       = "Functional Programming"
   description = "In computer science, functional programming is a programming paradigm where programs are constructed by applying and composing functions."
   avatar      = "${path.module}/avatar.png"
   avatar_hash = filesha256("${path.module}/avatar.png")
