295439eb113b9d94dd98631ed8398ae77dc6d010
Patrick Rice <Patrick.Rice@Kingland.com>
2022-05-04 01:07:08 +0000

    Add runner token to gitlab.rb

--- scripts/gitlab.rb
+++ scripts/gitlab.rb
@@ -7,3 +7,5 @@ registry_external_url 'http://127.0.0.1:5050'
 registry['enable']                    = true
 registry_nginx['ssl_certificate']     = "/etc/gitlab/ssl/gitlab-registry.pem"
 registry_nginx['ssl_certificate_key'] = "/etc/gitlab/ssl/gitlab-registry.key"
+
+gitlab_rails['initial_shared_runners_registration_token'] = "LoremIpsumDolorToken"
