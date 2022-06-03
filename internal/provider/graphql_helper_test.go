5a3bcada075469114cb83efd93188e87afbaad40
Patrick Rice <Patrick.Rice@Kingland.com>
2022-06-03 20:44:45 +0000

    Initial push of current user data source

--- internal/provider/graphql_helper_test.go
+++ internal/provider/graphql_helper_test.go
@@ -5,27 +5,24 @@ package provider
 
 import (
 	"context"
+	"log"
 	"testing"
 )
 
 func TestAcc_GraphQL_basic(t *testing.T) {
 
 	query := GraphQLQuery{
-		`query {currentUser {name}}`,
+		`query {currentUser {name, bot, gitpodEnabled, groupCount, id, namespace{id} publicEmail, username}}`,
 	}
 
 	var response CurrentUserResponse
-	_, _ = SendGraphQLRequest(context.Background(), testGitlabClient, query, &response)
+	_, err := SendGraphQLRequest(context.Background(), testGitlabClient, query, &response)
+	if err != nil {
+		log.Println(err)
+		t.Fail()
+	}
 
 	if response.Data.CurrentUser.Name != "Administrator" {
 		t.Fail()
 	}
-}
-
-type CurrentUserResponse struct {
-	Data struct {
-		CurrentUser struct {
-			Name string `json:"name"`
-		} `json:"currentUser"`
-	} `json:"data"`
 }
