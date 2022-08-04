705a44f3363a270019d55b11026a38166deb6b92
Shocktrooper <joseph3262@aol.com>
2022-08-04 15:36:25 +0000

    Added a second file for acceptance testing

--- internal/provider/data_source_gitlab_repository_tree_test.go
+++ internal/provider/data_source_gitlab_repository_tree_test.go
@@ -18,17 +18,30 @@ func TestAccDataSourceGitlabRepositoryTree_basic(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: fmt.Sprintf(`
+					resource "gitlab_repository_file" "foo" {
+						project        = "%[1]d"
+						file_path      = "testfile-meow"
+						branch         = "%[2]s"
+						content        = base64encode("Meow goes the cat")
+						commit_message = "feat: Meow"
+					}
+
 					data "gitlab_repository_tree" "this" {
-						project = %d
-						ref     = "%s"
+						project = %[1]d
+						ref     = gitlab_repository_file.foo.branch
 					}
 				`, testProject.ID, testProject.DefaultBranch),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.gitlab_repository_tree.this", "tree.#", "1"),
+					resource.TestCheckResourceAttr("data.gitlab_repository_tree.this", "tree.#", "2"),
 					resource.TestCheckResourceAttr("data.gitlab_repository_tree.this", "tree.0.name", "README.md"),
 					resource.TestCheckResourceAttr("data.gitlab_repository_tree.this", "tree.0.type", "blob"),
 					resource.TestCheckResourceAttr("data.gitlab_repository_tree.this", "tree.0.path", "README.md"),
 					resource.TestCheckResourceAttr("data.gitlab_repository_tree.this", "tree.0.mode", "100644"),
+
+					resource.TestCheckResourceAttr("data.gitlab_repository_tree.this", "tree.1.name", "testfile-meow"),
+					resource.TestCheckResourceAttr("data.gitlab_repository_tree.this", "tree.1.type", "blob"),
+					resource.TestCheckResourceAttr("data.gitlab_repository_tree.this", "tree.1.path", "testfile-meow"),
+					resource.TestCheckResourceAttr("data.gitlab_repository_tree.this", "tree.1.mode", "100644"),
 				),
 			},
 		},
