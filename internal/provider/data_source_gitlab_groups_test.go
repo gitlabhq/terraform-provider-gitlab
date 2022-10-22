c29127f1beda01ad62cfbfd7f61991bf87ea057f
Lars With <lars@with.de>
2022-10-22 20:12:17 +0200

    refactor test with helper function for groups

--- internal/provider/data_source_gitlab_groups_test.go
+++ internal/provider/data_source_gitlab_groups_test.go
@@ -7,124 +7,50 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func TestAccDataSourceGitlabGroups_basic(t *testing.T) {
-	rInt := acctest.RandInt()
-	rInt2 := acctest.RandInt()
+	prefixFoo := "acctest-group-foo"
+	groupsFoo := testAccCreateGroupsWithPrefix(t, 2, prefixFoo)
+
+	prefixLotsOf := "acctest-group-lotsof"
+	testAccCreateGroupsWithPrefix(t, 42, prefixLotsOf)
 
 	resource.ParallelTest(t, resource.TestCase{
 		ProviderFactories: providerFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourceGitlabGroupsConfig(rInt, rInt2),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("gitlab_group.foo1", "name", fmt.Sprintf("groups-name-foo1-%d", rInt)),
-					resource.TestCheckResourceAttr("gitlab_group.foo2", "name", fmt.Sprintf("groups-name-foo2-%d", rInt2)),
-				),
-			},
-			{
-				Config: testAccDataSourceGitlabGroupsConfigSort(rInt, rInt2),
+				Config: testAccDataSourceGitlabGroupsConfigSearchSort(prefixFoo),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("data.gitlab_groups.foos", "groups.#", "2"),
-					resource.TestCheckResourceAttr("data.gitlab_groups.foos", "groups.0.name", fmt.Sprintf("groups-name-foo1-%d", rInt)),
-					resource.TestCheckResourceAttr("data.gitlab_groups.foos", "groups.1.description", fmt.Sprintf("description-%d", rInt2)),
+					resource.TestCheckResourceAttr("data.gitlab_groups.foos", "groups.0.name", groupsFoo[0].Name),
 				),
 			},
 			{
-				Config: testAccDataSourceGitlabGroupsConfigSearch(rInt, rInt2),
+				Config: testAccDataSourceGitlabLotsOfGroupsSearch(prefixLotsOf),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.gitlab_groups.foos", "groups.#", "1"),
-				),
-			},
-			{
-				Config: testAccDataSourceGitlabLotsOfGroups(),
-			},
-			{
-				Config: testAccDataSourceGitlabLotsOfGroupsSearch(),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.gitlab_groups.foos", "groups.#", "42"),
+					resource.TestCheckResourceAttr("data.gitlab_groups.lotsof", "groups.#", "42"),
 				),
 			},
 		},
 	})
 }
 
-func testAccDataSourceGitlabGroupsConfig(rInt int, rInt2 int) string {
+func testAccDataSourceGitlabGroupsConfigSearchSort(prefix string) string {
 	return fmt.Sprintf(`
-resource "gitlab_group" "foo1" {
-  name = "groups-name-foo1-%d"
-  path = "groups-path-foo1-%d"
-  description = "description-%d"
-}
-
-resource "gitlab_group" "foo2" {
-  name = "groups-name-foo2-%d"
-  path = "groups-path-foo2-%d"
-  description = "description-%d"
-}
-	`, rInt, rInt, rInt, rInt2, rInt2, rInt2)
-}
-
-func testAccDataSourceGitlabGroupsConfigSort(rInt int, rInt2 int) string {
-	return fmt.Sprintf(`
-resource "gitlab_group" "foo1" {
-  name = "groups-name-foo1-%d"
-  path = "groups-path-foo1-%d"
-  description = "description-%d"
-}
-	
-resource "gitlab_group" "foo2" {
-  name = "groups-name-foo2-%d"
-  path = "groups-path-foo2-%d"
-  description = "description-%d"
-}
-	  
 data "gitlab_groups" "foos" {
   sort = "asc"
-  search = "groups-name-foo"
-  order_by = "name"
+  search = "%s"
+  order_by = "id"
 }
-	`, rInt, rInt, rInt, rInt2, rInt, rInt2)
+	`, prefix)
 }
 
-func testAccDataSourceGitlabGroupsConfigSearch(rInt int, rInt2 int) string {
+func testAccDataSourceGitlabLotsOfGroupsSearch(prefix string) string {
 	return fmt.Sprintf(`
-resource "gitlab_group" "foo1" {
-  name = "groups-name-foo1-%d"
-  path = "groups-path-foo1-%d"
-  description = "description-%d"
+data "gitlab_groups" "lotsof" {
+	search = "%s"
 }
-		
-resource "gitlab_group" "foo2" {
-  name = "groups-name-foo2-%d"
-  path = "groups-path-foo2-%d"
-  description = "description-%d"
-}
-	  
-data "gitlab_groups" "foos" {
-  search = "%d"
-}
-	`, rInt, rInt, rInt, rInt2, rInt2, rInt2, rInt2)
-}
-
-func testAccDataSourceGitlabLotsOfGroups() string {
-	return fmt.Sprintf(`
-resource "gitlab_group" "foo" {
-  name             = format("lots-group-%%02d", count.index+1)
-  description      = format("description-%%02d", count.index+1)
-  path             = format("lots-group-path-%%02d", count.index+1)
-  count            = 42
-}
-`)
-}
-
-func testAccDataSourceGitlabLotsOfGroupsSearch() string {
-	return fmt.Sprintf(`%v
-data "gitlab_groups" "foos" {
-	search = "lots"
-}
-	`, testAccDataSourceGitlabLotsOfGroups())
+	`, prefix)
 }
