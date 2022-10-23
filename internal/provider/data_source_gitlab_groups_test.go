d3e1fc801e05cf320efad42abda4b37cf6ee0400
Lars With <lars@with.de>
2022-10-23 10:45:28 +0200

    add validating group attributes

--- internal/provider/data_source_gitlab_groups_test.go
+++ internal/provider/data_source_gitlab_groups_test.go
@@ -5,6 +5,7 @@ package provider
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -24,7 +25,20 @@ func TestAccDataSourceGitlabGroups_basic(t *testing.T) {
 				Config: testAccDataSourceGitlabGroupsConfigSearchSort(prefixFoo),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("data.gitlab_groups.foos", "groups.#", "2"),
+					resource.TestCheckResourceAttr("data.gitlab_groups.foos", "groups.0.group_id", fmt.Sprint(groupsFoo[0].ID)),
+					resource.TestCheckResourceAttr("data.gitlab_groups.foos", "groups.0.full_path", groupsFoo[0].FullPath),
 					resource.TestCheckResourceAttr("data.gitlab_groups.foos", "groups.0.name", groupsFoo[0].Name),
+					resource.TestCheckResourceAttr("data.gitlab_groups.foos", "groups.0.full_name", groupsFoo[0].FullName),
+					resource.TestCheckResourceAttr("data.gitlab_groups.foos", "groups.0.web_url", groupsFoo[0].WebURL),
+					resource.TestCheckResourceAttr("data.gitlab_groups.foos", "groups.0.path", groupsFoo[0].Path),
+					resource.TestCheckResourceAttr("data.gitlab_groups.foos", "groups.0.description", groupsFoo[0].Description),
+					resource.TestCheckResourceAttr("data.gitlab_groups.foos", "groups.0.lfs_enabled", strconv.FormatBool(groupsFoo[0].LFSEnabled)),
+					resource.TestCheckResourceAttr("data.gitlab_groups.foos", "groups.0.request_access_enabled", strconv.FormatBool(groupsFoo[0].RequestAccessEnabled)),
+					resource.TestCheckResourceAttr("data.gitlab_groups.foos", "groups.0.visibility_level", fmt.Sprint(groupsFoo[0].Visibility)),
+					resource.TestCheckResourceAttr("data.gitlab_groups.foos", "groups.0.parent_id", fmt.Sprint(groupsFoo[0].ParentID)),
+					resource.TestCheckResourceAttr("data.gitlab_groups.foos", "groups.0.runners_token", groupsFoo[0].RunnersToken),
+					resource.TestCheckResourceAttr("data.gitlab_groups.foos", "groups.0.default_branch_protection", fmt.Sprint(groupsFoo[0].DefaultBranchProtection)),
+					resource.TestCheckResourceAttr("data.gitlab_groups.foos", "groups.0.prevent_forking_outside_group", strconv.FormatBool(groupsFoo[0].PreventForkingOutsideGroup)),
 				),
 			},
 			{
